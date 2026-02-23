1e9ecddac525bbd7f9bef94afd1f2d70da9b50a7
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-02-23 21:44:17 +0530

    added .vscode/Simpleintr.cs

---
+++ .vscode/Fibonacci.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of terms: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+        
+        int first = 0, second = 1;
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
