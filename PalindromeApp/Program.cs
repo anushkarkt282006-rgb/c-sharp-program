569a73466f3ce9d67cf8d94af4943eca9a430768
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-02-23 20:18:00 +0530

    Added p2.cs and deleted p1.cs and p2.cs. The commit message should reflect these changes.

---
+++ PalindromeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter a number: ");
+string input = Console.ReadLine();
+
+bool IsPalindrome(string num)
+{
+    string reversed = new string(num.Reverse().ToArray());
+    return num == reversed;
+}
+
+if (IsPalindrome(input))
+{
+    Console.WriteLine($"{input} is a palindrome.");
+}
+else
+{
+    Console.WriteLine($"{input} is not a palindrome.");
+}
+
